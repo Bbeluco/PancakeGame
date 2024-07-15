d744ab301eeacb5f5b9c8601559de35c6a0661d6
Bruno Beluco <beluco.bispo@hotmail.com>
2024-07-15 18:14:52 -0300

    refact: questionCreation class created

---
+++ classes/CriacaoPerguntas.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+
+//Private methods (this is priv bc the js do not exports this XD)
+function prepareQuestionStructure(perguntasAtuais, novaPergunta) {
+    if(!perguntasAtuais[novaPergunta["categoria"]]) {
+        perguntasAtuais[novaPergunta["categoria"]] = [];
+    }
+    
+    perguntasAtuais[novaPergunta["categoria"]].push({ 
+        "questao": novaPergunta["questao"],
+        "alternativas": novaPergunta["alternativas"],
+        "this.resposta": novaPergunta["this.resposta"],
+        "dificuldade": novaPergunta["dificuldade"]
+    })
+    
+    return perguntasAtuais;
+}
+
+module.exports = class CriacaoPerguntas {
+    fileName;
+    res;
+    constructor(fileName, res) {
+        this.fileName = fileName;
+        this.res = res;
+    }
+
+    createQuestion(req) {
+        let body = "";
+        req.on('data', (chunk) => {
+          body += chunk;
+        }).on('end', () => {
+          body = JSON.parse(body);
+          fs.readFile("./" + this.fileName, (error, content) => {
+            if(error) {
+              this.res.writeHead(500);
+              this.res.end('Erro ao tentar ler o arquivo ' + fileName)
+              return;
+            }
+            let perguntas = JSON.parse(content);
+            perguntas = prepareQuestionStructure(perguntas, body);
+            this.saveQuestion(perguntas)
+          })
+      
+          this.res.writeHead(200);
+          this.res.end();
+        });
+      }
+    
+    saveQuestion(perguntas) {
+        fs.writeFile("./" + this.fileName, JSON.stringify(perguntas), err => {
+            if(err) {
+                this.res.writeHead(500);
+                this.res.end('Erro ao escrever no arquivo ' + this.fileName);
+                return;
+            }
+        })
+    }
+}
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let http = require('http');
 let fs = require('fs');
-const { connect } = require('http2');
+const CriacaoPerguntas = require('./classes/CriacaoPerguntas');
 
 const PORT = 8000;
 
@@ -31,51 +31,10 @@ function returnFile(url, res) {
   });
 }
 
-function createQuestion(req, res) {
-  let body = "";
-  req.on('data', (chunk) => {
-    body += chunk;
-  }).on('end', () => {
-    body = JSON.parse(body);
-    let fileName = "perguntas.json"
-    fs.readFile("./" + fileName, (error, content) => {
-      if(error) {
-        res.writeHead(500);
-        res.end('Erro ao tentar ler o arquivo ' + fileName)
-        return;
-      }
-
-      let perguntasAtuais = JSON.parse(content);
-      if(!perguntasAtuais[body["categoria"]]) {
-        perguntasAtuais[body["categoria"]] = [];
-      }
-
-      perguntasAtuais[body["categoria"]].push({ 
-        "questao": body["questao"],
-        "alternativas": body["alternativas"],
-        "resposta": body["resposta"],
-        "dificuldade": body["dificuldade"]
-      })
-      
-      fs.writeFile("./" + fileName, JSON.stringify(perguntasAtuais), err => {
-        if(err) {
-          res.writeHead(500);
-          res.end('Erro ao escrever no arquivo ' + fileName);
-          return;
-        }
-      })
-    })
-
-  });
-
-  
-  res.writeHead(200);
-  res.end();
-}
-
 http.createServer((req, res) => {
   if(req.method == "POST" && req.url == "/save") {
-    createQuestion(req, res);
+    const criacaoPerguntas = new CriacaoPerguntas("perguntas.json", res);
+    criacaoPerguntas.createQuestion(req);
   } else {
     returnFile(req.url, res);
   }
